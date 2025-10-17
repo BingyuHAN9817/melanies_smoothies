89e5a16dfdcbf72c2d9d397dc79b3cf3f4974350
BingyuHAN9817 <103386778+BingyuHAN9817@users.noreply.github.com>
2025-10-17 17:54:43 +0200

    Retry

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # in the pd_df, use the FRUIT_NAME column, use the fruit chosen to get values from the SEARCH_ON column.
         # when the data is found, just give the first one.0 represents the first row. Of course, our table only has one raw per fruit name but this works
-        st.write('The search value for', fruit_chosen, 'is', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
