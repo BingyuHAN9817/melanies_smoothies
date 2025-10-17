f41a85cca40a2e05526835525d4ca324550a22f3
BingyuHAN9817 <103386778+BingyuHAN9817@users.noreply.github.com>
2025-10-17 17:46:27 +0200

    To get the "Search on" Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -36,6 +36,12 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # in the pd_df, use the FRUIT_NAME column, use the fruit chosen to get values from the SEARCH_ON column.
+        # when the data is found, just give the first one.0 represents the first row. Of course, our table only has one raw per fruit name but this works
+        st.write('The search value for', fruit_chosen, 'is', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
